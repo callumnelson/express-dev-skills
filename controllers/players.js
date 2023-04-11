1b8bf7a3abcc57953c021726c2d7c7da34476f40
Callum Nelson <callum.nelson95@gmail.com>
2023-04-11 14:27:39 -0400

    add players data and controller

---
+++ controllers/players.js
@@ -0,0 +1,12 @@
+import { players } from '../data/players-data.js'
+
+function index(req, res) {
+  res.render('players/index', {
+    players : players,
+    title : 'NBA Players'
+  })
+}
+
+export { 
+  index, 
+}
